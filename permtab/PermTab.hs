83cacb661e995e2f2f465d5be9e8102b6b490995
Daniel Wyckoff <dwyckoff76@hotmail.com>
2015-10-05 21:53:18 -0700

    In PermTab, In the middle of passageToPartition

--- permtab/PermTab.hs
+++ permtab/PermTab.hs
@@ -1,18 +1,35 @@
 module PermTab () where
 
-import Data.Array (Array(..), listArray)
+import Data.Array (Array(..), listArray, indices)
 import Data.List
+import Data.Set (Set)
+import qualified Data.Set as Set
 
-type UndEltType a = Array Int a
-type IntEltType = UndEltType Int
+{- This module will define functionality whereby a given "passage", i.e.
+   the indices of a list containing elements that are members of 
+   UndEltType is partitioned set-theoretically by the relation of having the 
+   same "note" or element of UndEltType -}
+type UndEltTypeArray a = Array Int a
+type IntEltType = UndEltTypeArray Int
 
+class UndEltType a where
+  undElts :: UndEltTypeArray a
+  member :: a->Bool
 
 twelveUndElts = listArray (0,11) [0..11]
-
 
 bounded :: Int -> Int -> Int -> Bool
 bounded n a b = a <= n && n <= b
 boundedTwelve n = bounded n 0 11
 
+--passageToPartition :: [a] -> [Set Int]
+{-
+passageToPartition l = foldl' (\n s->case containing n s of
+                                  Just a =  ) 
+                       [] (indices l) -}
 
-                                                                                       
+{-
+passageToPartition [] = [Set.empty]
+passageToPartition x:xs = UndEltType a => indices xs 
+                          
+-}
